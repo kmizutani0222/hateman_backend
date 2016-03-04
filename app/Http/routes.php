0d8fa76865e0eee889eb906e7045b1e0e713f451
水谷健太 <k.mizutani0222@gmail.com>
2016-03-04 20:43:40 +0900

    ルーティングを作成

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,5 +27,12 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+	// 認証のルート定義…
+	Route::get('auth/login', 'Auth\AuthController@getLogin');
+	Route::post('auth/login', 'Auth\AuthController@postLogin');
+	Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+	// 登録のルート定義…
+	Route::get('auth/register', 'Auth\AuthController@getRegister');
+	Route::post('auth/register', 'Auth\AuthController@postRegister');
 });
